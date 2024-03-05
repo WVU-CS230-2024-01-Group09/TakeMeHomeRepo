90fc5884740b5c9edb7bc567d751323586c57425
nsnyder11 <nps00011@mix.wvu.edu>
2024-03-05 15:24:13 -0500

    fixed login dev

--- take-me-home-app/src/App.js
+++ take-me-home-app/src/App.js
@@ -1,27 +1,31 @@
 import React from 'react';
-import './App.css'; 
+import './App.css'; // Make sure the path matches where your CSS file is located
 
 function App() {
     return (
-        <div className="bg"> {/* Background image */}
-            <div className="title">
-                <h1>Take Me Home Rideshare</h1>
-            </div>
-            <div className="container">
-                <h2>Welcome Mountaineers!</h2>
-                <h3>Log in:</h3>
-                <form id="loginForm">
-                    <input type="email" id="email" placeholder="Email" required />
-                    <input type="password" id="password" placeholder="Password" required />
-                    <input type="submit" value="Log in" />
-                </form>
-                <div className="links">
-                    <a href="#">Forgot password?</a> &mdash; <a href="#">Sign up</a>
+        <div className="App">
+            <div className="bg"></div> {/* Background image */}
+            <div className="content"> {/* Content wrapper */}
+                <div className="title">
+                    <h1>Take Me Home Rideshare</h1>
+                </div>
+                <div className="container">
+                    <h2>Welcome Mountaineers!</h2>
+                    <h3>Log in:</h3>
+                    <form id="loginForm">
+                        <input type="email" id="email" placeholder="Email" required />
+                        <input type="password" id="password" placeholder="Password" required />
+                        <input type="submit" value="Log in" />
+                    </form>
+                    <div className="links">
+                        <a href="#">Forgot password?</a> &mdash; <a href="#">Sign up</a>
+                    </div>
                 </div>
             </div>
         </div>
     );
 }
+
 
 export default App;
 
