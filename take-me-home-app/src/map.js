e66ad1427b9313783134e94a47664e9b26b40c43
your user name <your email>
2024-03-20 15:32:27 -0400

    map map map map map (initial commit)

---
+++ take-me-home-app/src/map.js
@@ -0,0 +1,21 @@
+import React, { Component } from "React";
+import { Map, Marker, GoogleApiWrapper } from "google-maps-react";
+
+class App extends Component {
+    render() {
+    return (
+    <div className="Map">
+        <Map google={this.props.google}
+        zoom={10}
+        initialCenter={{
+        lat: 39.635058,
+        lng: -79.954278
+        }}/>
+    </div>
+    );
+    }
+   }
+   
+   export default GoogleApiWrapper({
+    apiKey:("AIzaSyBnqgu12a6mp8IJM1_8WzKD1tFszpvULHY")
+   })(Map);
