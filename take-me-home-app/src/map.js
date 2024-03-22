2e49e3fe5bd3136373487cf5cbcbd00c92175384
shoalstone <jmd00009@mix.wvu.edu>
2024-03-22 15:26:09 -0400

    fixed map namespace conflict

--- take-me-home-app/src/map.js
+++ take-me-home-app/src/map.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Map, Marker, GoogleApiWrapper } from "google-maps-react";
 
-class Map extends Component {
+class GoogleMap extends Component {
     render() {
     return (
     <div className="Map">
@@ -18,4 +18,4 @@ class Map extends Component {
    
    export default GoogleApiWrapper({
     apiKey:("AIzaSyBnqgu12a6mp8IJM1_8WzKD1tFszpvULHY")
-   })(Map);
+   })(GoogleMap);
