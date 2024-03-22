c2fdd8b853fdcc62692df11572b067e144f19ff9
nsnyder11 <nps00011@mix.wvu.edu>
2024-03-22 15:38:25 -0400

    working login. create will be implemented

--- take-me-home-app/src/App.js
+++ take-me-home-app/src/App.js
@@ -1,29 +1,24 @@
+// App.js
 import React from 'react';
-import './App.css'; // Importing the CSS file for styling
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Login';
+import HomePage from './HomePage';
+import { ProvideAuth } from './useAuth';
 
 function App() {
     return (
-        <div className="App">
-            <div className="bg"></div> {/* Background image */}
-            <div className="content"> {/* Content container for centering */}
-                <div className="title">
-                    <h1>Take Me Home Rideshare</h1>
-                </div>
-                <div className="container"> {/* Login container */}
-                    <h2>Welcome Mountaineers!</h2>
-                    <h3>Log in:</h3>
-                    <form id="loginForm">
-                        <input type="email" id="email" placeholder="Email" required />
-                        <input type="password" id="password" placeholder="Password" required />
-                        <input type="submit" value="Log in" />
-                    </form>
-                    <div className="links">
-                        <a href="#">Forgot password?</a> &mdash; <a href="#">Sign up</a>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <ProvideAuth>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="/home" element={<HomePage />} />
+                </Routes>
+            </Router>
+        </ProvideAuth>
     );
 }
 
 export default App;
+
+
+
---
+++ take-me-home-app/src/HomePage.js
@@ -0,0 +1,13 @@
+// HomePage.js
+import React from 'react';
+
+function HomePage() {
+    return (
+        <div>
+            <h1>Welcome to the Home Page!</h1>
+            <p>This is a protected page that requires authentication.</p>
+        </div>
+    );
+}
+
+export default HomePage;
