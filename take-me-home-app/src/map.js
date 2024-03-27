f52f4879f213a582c1b907702d17f36b086ffb2f
lmf2003 <ef00023@mix.wvu.edu>
2024-03-27 15:34:11 -0400

    uncomment

--- take-me-home-app/src/map.js
+++ take-me-home-app/src/map.js
@@ -1,4 +1,4 @@
-/*import React, { Component } from "react";
+import React, { Component } from "react";
 import { Map, Marker, GoogleApiWrapper } from "google-maps-react";
 import { Wrapper } from "@googlemaps/react-wrapper";
 
@@ -21,7 +21,7 @@ import { Wrapper } from "@googlemaps/react-wrapper";
     apiKey:("AIzaSyBnqgu12a6mp8IJM1_8WzKD1tFszpvULHY")
    })(GoogleMap); */
 
-  /* let map;
+   let map;
 
 async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
@@ -38,4 +38,4 @@ const MyApp = () => (
   </Wrapper>
 );
 
-initMap(); */
+initMap(); 
