7665d4784f2299421f9bfb6cd2834853de37c034
lmf2003 <ef00023@mix.wvu.edu>
2024-04-10 15:25:39 -0400

    some homepage and also the logo

--- take-me-home-app/src/HomePage.js
+++ take-me-home-app/src/HomePage.js
@@ -1,12 +1,15 @@
 // HomePage.js
 import React from 'react';
 import './styles.css'; // Importing the CSS file for styling
+import './map.js';
+import ',/map.html';
 
 function HomePage() {
     return (
         <div>
             <h1>Welcome to the Home Page!</h1>
-            <p>This is a protected page that requires authentication.</p>
+            <h3>About:</h3>
+            <p style="background-image: url('logo.png');">Take Me Home is a rideshare service created with the purpose of providing students at West Virginia University a means to get or provide a ride home for break.</p>
         </div>
     );
 }
